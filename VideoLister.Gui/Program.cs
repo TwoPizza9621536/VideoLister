4cb5e6e0af6147a7d96643e8dd946fd16fe4d463
Samuel Wu 吴申榕 <twopizza9621536@gmail.com>
2022-01-21 16:30:19 -0500

    Move TestGUI to its own GUI folder and start GUI development
    Signed-off-by: Samuel Wu 吴申榕 <twopizza9621536@gmail.com>

---
+++ VideoLister.Gui/Program.cs
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: 2021-2022 The Video Lister Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+using Gtk;
+using System;
+
+namespace VideoLister.Gui;
+
+internal class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application.Init();
+
+        var App = new Application("org.VideoLister.Gui", GLib.ApplicationFlags.None);
+        App.Register(GLib.Cancellable.Current);
+
+        var Win = new MainWindow();
+        App.AddWindow(Win);
+
+        var menu = new GLib.Menu();
+        menu.AppendItem(new GLib.MenuItem("Help", "app.help"));
+        menu.AppendItem(new GLib.MenuItem("About", "app.about"));
+        menu.AppendItem(new GLib.MenuItem("Quit", "app.quit"));
+        App.AppMenu = menu;
+
+        var AboutAction = new GLib.SimpleAction("About", null);
+        AboutAction.Activated += AboutActivated;
+        App.AddAction(AboutAction);
+
+        Win.ShowAll();
+        Application.Run();
+    }
+
+    private static void AboutActivated(object sender, EventArgs e)
+    {
+        var about = new AboutDialog();
+        about.Show();
+        about.Close();
+    }
+}
